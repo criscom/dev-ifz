28a51fd6329d3b5e05457e3afe03ff4e5e6eeac3
Christoph Potzinger <criscom27@gmail.com>
2015-07-29 12:18:41 +0200

    Remove icons, increase base font size, and further adjustments according to meeting with Mr Suschek and Mrs Jahrbacher.
     On branch master
     Changes to be committed:
    	modified:   js/script.js
    	modified:   sass/library/_image.scss
    	modified:   sass/library/_placeholders.scss
    	modified:   sass/sl-components/_default.scss
    	modified:   sass/sl-components/_header.scss
    	modified:   sass/sl-components/_team-landing.scss
    	modified:   templates/block/block--block--3.tpl.php
    	new file:   templates/block/block--block--3.tpl.php.bak
    	modified:   templates/block/block--menu-block--2.tpl.php
    	new file:   templates/block/block--menu-block--2.tpl.php.bak
    	modified:   templates/block/block--menu-block--3.tpl.php
    	new file:   templates/block/block--menu-block--3.tpl.php.bak
    	modified:   templates/block/block--views--news-block-block.tpl.php
    	new file:   templates/block/block--views--news-block-block.tpl.php.bak
    	modified:   templates/block/block--views--publikationen-block-1.tpl.php

--- js/script.js
+++ js/script.js
@@ -14,13 +14,15 @@
 (function ($) {
   $(document).ready(function() { 
     // add glyphicon ok icon in front of publication list in box
-    $('#block-views-publikationen-block-1 .view ul li a').prepend('<span class="glyphicon glyphicon-ok"></span>');
+   
+   // $('#block-views-publikationen-block-1 .view ul li a').prepend('<span class="glyphicon glyphicon-ok"></span>');
     
     // add glyphicon user icon in front of team member list in box
-    $('.sidebar .view-team .views-row span.field-content a').prepend('<span class="glyphicon glyphicon-ok"></span>');
+  
+  //  $('.sidebar .view-team .views-row span.field-content a').prepend('<span class="glyphicon glyphicon-ok"></span>');
 
     // add glyphicon user icon in front of team member list in box
-    $('#block-views-news-block-block .views-row span.field-content a').prepend('<span class="glyphicon glyphicon-ok"></span>');
+  //  $('#block-views-news-block-block .views-row span.field-content a').prepend('<span class="glyphicon glyphicon-ok"></span>');
 
         // add glyphicon ok icon in front of Forschung menu list in box
     // $('#block-menu-block-2 ul.menu li a').prepend('<span class="glyphicon glyphicon-ok"></span>');
@@ -28,7 +30,7 @@
      // add glyphicon ok icon in front of Forschung menu list in box
     // $('#block-menu-block-3 ul.menu li a').prepend('<span class="glyphicon glyphicon-ok"></span>');
 
-        $('div[id^="block-menu-block-"] ul.menu li a').prepend('<span class="glyphicon glyphicon-ok"></span>');
+     //   $('div[id^="block-menu-block-"] ul.menu li a').prepend('<span class="glyphicon glyphicon-ok"></span>');
 
 
     // add glyphicon download icon for download files
--- templates/block/block--menu-block--2.tpl.php
+++ templates/block/block--menu-block--2.tpl.php
@@ -14,7 +14,7 @@
   <?php print render($title_prefix); ?>
   <?php if ($title): ?>
     
-    <h2<?php print $title_attributes; ?>><span class="halflings halflings-star"></span><?php print $title; ?></h2>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
